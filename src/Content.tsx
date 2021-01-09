12d48cb97c115445b6915631e8727a95ac3a2274
Nikita Inskorostinskii <62565112+nikita11044@users.noreply.github.com>
2021-01-09 12:09:32 +0300

    Init

---
+++ src/Content.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Content() {
+    return (
+        <div className='content'>
+            Main Content
+        </div>
+    );
+}
+
+export default Content;
---
+++ src/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav className='nav'>
+            <ul>
+                <li>Profile</li>
+                <li>Messages</li>
+                <li>News</li>
+                <li>Music</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
